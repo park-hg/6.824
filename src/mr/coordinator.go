b649bf47e4fb0fc010dfa3a2b9802912174331f6
park-hg <jogatpark@gmail.com>
2023-10-29 17:24:15 +0900

    implement map worker

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -1,33 +1,75 @@
 package mr
 
-import "log"
-import "net"
-import "os"
-import "net/rpc"
-import "net/http"
-
+import (
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"os"
+	"sync"
+)
 
 type Coordinator struct {
 	// Your definitions here.
+	MapTaskInfo    map[string]WorkerStatus
+	ReduceTaskInfo []bool
 
+	mu sync.Mutex
 }
+
+type WorkerStatus struct {
+	ID        int
+	Status    Status
+	Locations []string
+}
+
+type Status string
+
+const (
+	Idle       = Status("idle")
+	InProgress = Status("in-progress")
+	Completed  = Status("completed")
+)
 
 // Your code here -- RPC handlers for the worker to call.
 
-//
 // an example RPC handler.
 //
 // the RPC argument and reply types are defined in rpc.go.
-//
 func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
 	reply.Y = args.X + 1
 	return nil
 }
 
+func (c *Coordinator) GetMapTask(args *AskMapTaskArgs, reply *AskMapTaskReply) error {
+	for filename, status := range c.MapTaskInfo {
+		c.mu.Lock()
+		if status.Status == Idle {
+			reply.Filename = filename
+			reply.Buckets = len(c.ReduceTaskInfo)
 
-//
+			c.MapTaskInfo[filename] = WorkerStatus{ID: args.WorkerID, Status: InProgress}
+			c.mu.Unlock()
+			break
+		}
+		c.mu.Unlock()
+	}
+	return nil
+}
+
+func (c *Coordinator) CompleteMapTask(args *CompleteMapTaskArgs, reply *CompleteMapTaskReply) error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if c.MapTaskInfo[args.Filename].Status == Completed {
+		reply.Error = nil
+		return nil
+	}
+	c.MapTaskInfo[args.Filename] = WorkerStatus{ID: args.WorkerID, Status: Completed, Locations: args.Locations}
+	reply.Error = nil
+	return nil
+}
+
 // start a thread that listens for RPCs from worker.go
-//
 func (c *Coordinator) server() {
 	rpc.Register(c)
 	rpc.HandleHTTP()
@@ -41,29 +83,28 @@ func (c *Coordinator) server() {
 	go http.Serve(l, nil)
 }
 
-//
 // main/mrcoordinator.go calls Done() periodically to find out
 // if the entire job has finished.
-//
 func (c *Coordinator) Done() bool {
 	ret := false
 
 	// Your code here.
 
-
 	return ret
 }
 
-//
 // create a Coordinator.
 // main/mrcoordinator.go calls this function.
 // nReduce is the number of reduce tasks to use.
-//
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
 	c := Coordinator{}
 
 	// Your code here.
-
+	c.MapTaskInfo = make(map[string]WorkerStatus, len(files)-1)
+	for _, f := range files[1:] {
+		c.MapTaskInfo[f] = WorkerStatus{ID: -1, Status: Idle}
+	}
+	c.ReduceTaskInfo = make([]bool, nReduce, nReduce)
 
 	c.server()
 	return &c
--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,48 +1,78 @@
 package mr
 
-import "fmt"
-import "log"
-import "net/rpc"
-import "hash/fnv"
+import (
+	"fmt"
+	"hash/fnv"
+	"io"
+	"log"
+	"net/rpc"
+	"os"
+	"strings"
+)
 
-
-//
 // Map functions return a slice of KeyValue.
-//
 type KeyValue struct {
 	Key   string
 	Value string
 }
 
-//
 // use ihash(key) % NReduce to choose the reduce
 // task number for each KeyValue emitted by Map.
-//
 func ihash(key string) int {
 	h := fnv.New32a()
 	h.Write([]byte(key))
 	return int(h.Sum32() & 0x7fffffff)
 }
 
-
-//
 // main/mrworker.go calls this function.
-//
 func Worker(mapf func(string, string) []KeyValue,
 	reducef func(string, []string) string) {
 
 	// Your worker implementation here.
 
 	// uncomment to send the Example RPC to the coordinator.
-	// CallExample()
+	filename, buckets := CallMapTaskAsk()
+	if filename == "" {
+		return
+	}
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatalf("cannot open %v", filename)
+	}
+	content, err := io.ReadAll(file)
+	if err != nil {
+		log.Fatalf("cannot read %v", filename)
+	}
+	file.Close()
+	kva := mapf(filename, string(content))
 
+	//TODO: create new file to store intermediates and send that the job finished to the coordinator
+	filetitle := strings.Split(filename, ".")[0]
+	dir := fmt.Sprintf("m-intermediate-%s", filetitle)
+	os.Mkdir(dir, 0777)
+
+	intermediates := make([]*os.File, buckets)
+	locations := make([]string, buckets)
+	for i := 0; i < buckets; i++ {
+		iname := fmt.Sprintf("%s/out-%d", dir, i)
+		ofile, _ := os.Create(iname)
+		intermediates[i] = ofile
+		locations[i] = iname
+	}
+
+	for i := range kva {
+		fmt.Fprintf(intermediates[i%buckets], fmt.Sprintf("%s %s\n", kva[i].Key, kva[i].Value))
+	}
+	for _, f := range intermediates {
+		f.Close()
+	}
+
+	CallMapTaskComplete(filename, locations)
 }
 
-//
 // example function to show how to make an RPC call to the coordinator.
 //
 // the RPC argument and reply types are defined in rpc.go.
-//
 func CallExample() {
 
 	// declare an argument structure.
@@ -61,11 +91,25 @@ func CallExample() {
 	fmt.Printf("reply.Y %v\n", reply.Y)
 }
 
-//
+func CallMapTaskAsk() (string, int) {
+	args := AskMapTaskArgs{WorkerID: os.Getpid()}
+	reply := AskMapTaskReply{}
+	call("Coordinator.GetMapTask", &args, &reply)
+
+	return reply.Filename, reply.Buckets
+}
+
+func CallMapTaskComplete(filename string, locations []string) error {
+	args := CompleteMapTaskArgs{WorkerID: os.Getpid(), Filename: filename, Locations: locations}
+	reply := CompleteMapTaskReply{}
+	call("Coordinator.CompleteMapTask", &args, &reply)
+
+	return reply.Error
+}
+
 // send an RPC request to the coordinator, wait for the response.
 // usually returns true.
 // returns false if something goes wrong.
-//
 func call(rpcname string, args interface{}, reply interface{}) bool {
 	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
 	sockname := coordinatorSock()
